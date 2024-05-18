38887b0048fef3851ed905c9d8e2362d1eccd3f3
RianRefanza06 <rianrifanzah4@gmail.com>
2024-05-18 19:37:27 +0700

    first commit

---
+++ simpanmahasiswa.php
@@ -0,0 +1,16 @@
+<?php
+        //include koneksi
+        include "DB.php";
+        //buat sql
+        $sql="INSERT INTO `mahasiswa` (`nim`, `nama`, `alamat`, `jkl`, `agama`, `email`,
+         `pwd`, `created_at`, `update_at`) VALUES ('123457', 'pia', 'padang', 
+         'perempuan', 'kristen', 'pia@gmail.com', '123456', current_timestamp(), '2024-04-22 05:13:36.000000')";
+
+        //proses sql ke database
+        $q=mysqli_query($koneksi,$sql)or die('SQL_error()');
+        if($q){
+            echo "Data Berhasil disimpan";
+        }else{
+            echo "Gagal menyimpan data! "  . mysqli_error();
+        }
+?>
